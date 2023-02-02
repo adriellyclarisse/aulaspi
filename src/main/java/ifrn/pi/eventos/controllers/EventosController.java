813f0df0b3fc22887a58c6b9cfafede091c2eaf9
Adrielly <adriellyclarisse2018@gmail.com>
2023-02-02 17:48:00 -0300

    entregueeeeeiii

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -3,13 +3,17 @@ package ifrn.pi.eventos.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ifrn.pi.eventos.models.Convidado;
 import ifrn.pi.eventos.models.Evento;
@@ -31,11 +35,16 @@ public class EventosController {
 	}
 
 	@PostMapping
-	public String salvar(Evento evento) {
-
+	public String salvar(@Valid Evento evento, BindingResult result, RedirectAttributes attributes) {
+		
+		if(result.hasErrors()) {
+			return form(evento);
+		}
+		
 		System.out.println(evento);
 		er.save(evento);
-
+		attributes.addFlashAttribute("mensagem", "Evento salvo com sucesso!");
+		
 		return "redirect:/eventos";
 	}
 
@@ -133,7 +142,7 @@ public class EventosController {
 	}
 	
 	@GetMapping("/{id}/remover")
-	public String apagarEvento(@PathVariable Long id) {
+	public String apagarEvento(@PathVariable Long id, RedirectAttributes attributes) {
 		
 		Optional<Evento> opt = er.findById(id);
 		
@@ -143,8 +152,8 @@ public class EventosController {
 			List<Convidado> convidados = cr.findByEvento(evento);
 			
 			cr.deleteAll(convidados);
-			
 			er.delete(evento);
+			attributes.addFlashAttribute("mensgem", "Evento removido com sucesso!");
 		}
 		
 		return "redirect:/eventos";
